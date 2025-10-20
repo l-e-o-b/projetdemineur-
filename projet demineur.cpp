f99da8370ab60e312318a8d07c61bc4bdd71c1d6
unknown <lbouyat@gamingcampus.local>
2025-10-20 17:57:00 +0200

    hello

--- projet demineur.cpp
+++ projet demineur.cpp
@@ -9,19 +9,24 @@ const int facile = 9;
 int NbMines = (facile * facile) * 0.15;  //calcule de 15% de mines
 
 // initialiser le tableau pour le plateau de jeu
-void print_tab(int tab[facile][facile], bool CaseVisible[facile][facile])
+void print_tab(int tab[facile][facile], bool CaseVisible[facile][facile], bool caseFlag[facile][facile])
 {
 	for (int i = 0; i < 9; i++)
 	{
 		for(int j = 0; j < 9; j++)
 		{
-			if (tab[i][j] == -1 && CaseVisible[i][j] == true)
+			if (CaseVisible[i][j] == true)
 			{
-				cout << "[*]";
+				if (tab[i][j] == -1)
+					cout << "[*]";
+				else
+				{
+					cout << "[" << tab[i][j] << "]";
+				}
 			}
-			else if(tab[i][j] != -1 && CaseVisible[i][j] == false)
+			else if (caseFlag[i][j] == true)
 			{
-				cout << "[?]";
+				cout << "[F]";
 			}
 			else
 			{
@@ -50,31 +55,102 @@ void PlacerMine(int tab[facile][facile], bool CaseVisible[facile][facile])
 }
 
 
-bool ChoisirCase(int tab[facile][facile], bool CaseVisible[facile][facile])
+bool ChoisirCase(int tab[facile][facile], bool CaseVisible[facile][facile], int& i, int& j)
 {
-	int i, j;
 	cout << "donner la ligne de la case" << endl;
 	cin >> i;
 	cout << "donne la colonne de la case" << endl;
 	cin >> j;
-	if (i >= 0 && i < 9 && j >= 0 && j < 9)
+	if (i < 0 || i >= 9 || j < 0 || j >= 9)
 	{
-		CaseVisible[i][j] = true;
+		cout << "coordonnees invalides !" << endl;
+		return 1;  
 	}
 	if (tab[i][j] == -1)
 	{
-		return 0;
+		return 0; 
 	}
 	return 1;
 }
 
 void CaseAutour(int tab[facile][facile])
 {
+	int Boom = 0;
+	int NbCase = 0;
+	int ni = 0;
+	int nj = 0;
 	for (int i = 0; i < 9; i++)
 	{
 		for (int j = 0; j < 9; j++)
 		{
+			if (tab[i][j] != -1)
+			{
+				Boom = 0;
+				for (int dx = -1; dx <= 1; dx++)
+				{
+					for (int dy = -1; dy <= 1; dy++)
+					{
+						if (!(dx == 0 && dy == 0))
+						{
+							ni = i + dx;
+							nj = j + dy;
+							if (ni >= 0 && ni < 9 && nj < 9 && nj >= 0)
+							{
+								if (tab[ni][nj] == -1)
+								{
+									Boom++;
+								}
+							}
+						}
+					}
+				}
+				tab[i][j] = Boom;
+			}
+		}
+	}
+}
 
+
+
+void ReveleCase(int tab[facile][facile], bool CaseVisible[facile][facile], int i, int j)
+{
+	if (i < 9 && i >= 0 && j < 9 && j >= 0)
+	{
+		if (CaseVisible[i][j] == false)
+		{
+			CaseVisible[i][j] = true;
+			if (tab[i][j] == 0)
+			{
+				for (int dx = -1; dx <= 1; dx++)
+				{
+					for (int dy = -1; dy <= 1; dy++)
+					{
+						if (!(dx == 0 && dy == 0))
+						{
+							int ni = i + dx;
+							int nj = j + dy;
+							ReveleCase(tab, CaseVisible, ni, nj);
+						}
+					}
+				}
+			}
+		}
+	}
+	else
+	{
+
+	}
+}
+
+
+void ReveleMines(int tab[facile][facile], bool CaseVisible[facile][facile])
+{
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			if (tab[i][j] == -1)
+				CaseVisible[i][j] = true;
 		}
 	}
 }
@@ -86,25 +162,66 @@ int main()
 	int tab[facile][facile];
 	bool CaseVisible[facile][facile];
 	bool gameover = false;
+	int i, j;
+	bool caseFlag[facile][facile];
+	int choix;
 
-	for (int i = 0; i < 9; i++)
-	{
-		for (int j = 0; j < 9; j++)
+
+		for (int i = 0; i < 9; i++)
 		{
-			tab[i][j] = 0;
-			CaseVisible[i][j] = false;
+			for (int j = 0; j < 9; j++)
+			{
+				tab[i][j] = 0;
+				CaseVisible[i][j] = false;
+				caseFlag[i][j] = false;
+			}
 		}
-	}
-	srand(time(NULL)); // obligatoire pour aleatoire sans = le meme nombre a chaque tours 
-	PlacerMine(tab,CaseVisible);
-	while (gameover == false)
-	{
-		if (ChoisirCase(tab, CaseVisible) == 0)
+		srand(time(NULL)); // obligatoire pour aleatoire sans = le meme nombre a chaque tours 
+		PlacerMine(tab,CaseVisible);
+		CaseAutour(tab);
+		while (gameover == false)
 		{
-			gameover = true;
-			cout << "GAMEOVER" << endl;
+			print_tab(tab, CaseVisible, caseFlag);
+			cout << "vous souhaiter :" << endl;
+			cout << "1.reveler une case" << endl;
+			cout << "2. mettre/retirer un drapeau" << endl;
+
+			cin >> choix;
+
+			if (choix == 1)
+			{
+				if (ChoisirCase(tab, CaseVisible, i, j) == 0)
+				{
+					gameover = true;
+					ReveleMines(tab, CaseVisible);
+					print_tab(tab, CaseVisible, caseFlag);
+					cout << "GAMEOVER" << endl << "vous avez toucher une mine a la case : " << "(" << i << "," << j << ")" << endl;
+
+				}
+				else
+				{
+					ReveleCase(tab, CaseVisible, i, j);
+				}
+			}
+			else if (choix == 2)
+			{
+				cout << "donner la ligne de la case pour poser/enlever un drapeau" << endl;
+				cin >> i;
+				cout << "donner la colonne de la case pour poser/enlever un drapeau" << endl;
+				cin >> j;
+				if (i < 0 || i >= 9 || j < 0 || j >= 9)
+				{
+					cout << "coordonnees invalides" << endl;
+				}
+				else if (CaseVisible[i][j])
+				{
+					cout << "impossible de poser un drapeau sur une case revele" << endl;
+				}
+				else
+				{
+					caseFlag[i][j] = !caseFlag[i][j];
+				}
+			}
 		}
-		print_tab(tab, CaseVisible);
-	}
-	
+	return 0;
 }
